6917a05452410807d41df25eb3a8d81fdcec9f46
LeviSolomon270 <solomonlevi270@gmail.com>
2026-07-23 16:28:25 +0200

    iamge

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={require('./assets/phone.png')} style={styles.image} />
+      <Text style={styles.welcomeTxt}>Welcome to my app</Text>
       <Text>Welcome to my app</Text>
-      <Text>Enter Name</Text>
+      <Text style={styles.boldText}>Enter Name</Text>
       <TextInput placeholder="Bob"/>
-      <Text>Enter Surname:</Text>
+      <View style={styles.divider} />
+      <Text style={styles.boldText}>Enter Surname:</Text>
       <TextInput placeholder="Burger"/>
 
       <Button title="Add User" />
@@ -18,11 +21,29 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   welcomeTxt: {
     paddingTop: 50,
     color: 'blue',
     fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: 'black',
+    marginVertical: 15,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
   },
 });
